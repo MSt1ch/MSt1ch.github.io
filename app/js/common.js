5513de43224f3733696dd78e1f599fcb9aab229e
St1ch <st1ch@mail.ru>
2017-12-05 00:21:42 +0300

    restyle main page
    remake all style and fix bugs

--- app/js/common.js
+++ app/js/common.js
@@ -16,13 +16,52 @@ $(function() {
 	$('.hamburger').on( "click", function() {
 		var api = $('#my-menu').data('mmenu');
 		var ths = $(this)
-   api.bind('open:finish', function() {
-		ths.addClass('is-active')
-	});
-	api.bind('close:finish', function() {
-		ths.removeClass('is-active');
-	});
+		api.bind('open:finish', function() {
+			ths.addClass('is-active')
+		});
+		api.bind('close:finish', function() {
+			ths.removeClass('is-active');
+		});
 	});
 	
+	$('.go-to-top').click(function() {
+		$(window.opera ? 'html' : 'html, body').animate({
+			scrollTop: 0
+		}, 'slow');
+	});
+
+	function onScroll(event){
+
+		var scrollPos = $(document).scrollTop();
+		if(scrollPos <= 400 || scrollPos >= 1000){
+			$('.skills-wrap-list em').removeClass('animate-icon');
+			
+		}
+		else{ 
+			$('.skills-wrap-list em').addClass('animate-icon');
+		};
+		
+
+		if(scrollPos >= 1100){
+			$(".go-to-top").fadeIn('normal');
+		} else{
+			$(".go-to-top").fadeOut('normal')
+		}
+
+
+
+	}
+
+	$(document).on("scroll", onScroll);
+
+
+	$(".main-nav_link, .scroll-button_link").on('click', function(e) {
+     e.preventDefault();
+     var target = $(this).attr('href');
+     $('html, body').animate({
+       scrollTop: ($(target).offset().top) - 100
+     }, 1000);
+  });
+
 
 });
